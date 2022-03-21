a5ca9984660ce7f1571aa370fc61509fe520e4e8
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-03-21 14:48:38 +1300

    ALl the work so far

--- ark/src/App.js
+++ ark/src/App.js
@@ -5,16 +5,19 @@ import Welcome from './Components/Welcome';
 import Hello from './Components/Hello';
 import Message from './Components/Message';
 import Test from './Components/Test';
+import Navigation from './Components/Navigation';
 function App() {
   return (
     <div className="App">
+      
      {/*Since there is no content between tags can have it self clossing */}
      <Greet name="User"><p>This is the childrens prop</p></Greet>
      <Welcome name="User"><p>Some random child writing</p></Welcome>
      <Hello/>
      <Message></Message>
     <Test></Test>
-    </div>
+    <Navigation/>
+   </div>
   );
 }
 
---
+++ ark/src/Components/Navigation.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import './Navigation.scss'
+class Navigation extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>Profile</li>
+                    <li>Comunication</li>
+                    <li>Boss</li>
+                </ul>
+            </div>
+        )
+    }
+}
+export default Navigation

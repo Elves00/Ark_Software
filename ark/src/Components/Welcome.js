4ff28767b1c16b71247d9cf1942528bee6ddb993
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-03-23 10:43:38 +1300

    Finished app

--- ark/src/Components/Welcome.js
+++ ark/src/Components/Welcome.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 class Welcome extends Component {
     render() {
+
+
+
+
+
+        
         return (
             <div>
                 <h1>Welcome To Ark {this.props.name}</h1>

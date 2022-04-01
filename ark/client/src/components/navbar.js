7d48b9f3ecd6def3884d6ec4cb302319a028ccd2
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-04-02 08:45:56 +1300

    React image in navbar

--- ark/client/src/components/navbar.js
+++ ark/client/src/components/navbar.js
@@ -5,14 +5,15 @@ import "bootstrap/dist/css/bootstrap.css";
 
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
-
+//Import image
+import card from '../logo192.png'
 // Here, we display our Navbar
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink className="navbar-brand" to="/">
-        <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+          <img src={card} alt="Avatar" style={{ "width": 25 + '%' }} />
         </NavLink>
         <button
           className="navbar-toggler"
@@ -28,7 +29,7 @@ export default function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
+            <li className="nav-item">
               <NavLink className="nav-link" to="/bossPage">
                 RAID!!
               </NavLink>
@@ -43,7 +44,7 @@ export default function Navbar() {
               <NavLink className="nav-link" to="/createUser">
                 Create User
               </NavLink>
-              
+
             </li>
           </ul>
         </div>

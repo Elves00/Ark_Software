6e6c04f3445a987776c03bdacfa095a2c62a7718
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-04-05 19:33:09 +1200

    Home page
    A home page which tracks how wide the screen is using useeffect hook

--- ark/client/src/components/homePage.js
+++ ark/client/src/components/homePage.js
@@ -1,11 +1,35 @@
-import React from "react";
+import "./homePage.css"
+import React, { useEffect, useState } from "react";
 
 export default function HomePage() {
+
+    //The state width and how to set it plus intial condition
+    const [width, setWidth] = useState(window.innerWidth);
+
+    
+    //Use effect re-sets the width state every time there is a window event
+   useEffect(()=>{
+       function handleResize(){
+           setWidth(window.innerWidth);
+       }
+       window.addEventListener("resize", handleResize);
+       return () => window.removeEventListener("resize", handleResize);
+
+   },[]);
+      
+
     return (
         <div>
             <h1>Lost Ark</h1>
             <h2>Popular Raids</h2>
+
             <h2>Popular Pages</h2>
+
+             {/*Width is the state*/}
+          
+            <p>You clicked {width} times</p>
+           
+            
         </div>
     );
 }

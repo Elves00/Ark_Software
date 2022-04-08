369315dfb40191c142cdeb94e87cabee5a3e1a6d
Davis Djaja <davisdjaja20@gmail.com>
2022-04-08 19:58:26 +1200

    register page

--- ark/client/src/components/login.js
+++ ark/client/src/components/login.js
@@ -4,11 +4,14 @@ import "./user.css";
 
 function login() {
   return (
-    <form className="form">
-      <Input type="text" placeholder="Username" />
-      <Input type="password" placeholder="Password" />
-      <button type="Submit">Login</button>
-    </form>
+    <div className="form">
+      <h3>Login</h3>
+      <form>
+        <Input type="text" placeholder="Username" />
+        <Input type="password" placeholder="Password" />
+        <button type="Submit">Login</button>
+      </form>
+    </div>
   );
 }
 

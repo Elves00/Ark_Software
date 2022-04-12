f4f3d2b7ae58c3f43bab4379dbf772117f25c7d7
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-04-12 12:53:12 +1200

    Server update?

--- ark/server/server.js
+++ ark/server/server.js
@@ -15,12 +15,8 @@ app.use(require("./routes/record"));
 
 //Allow post method for these paths?? using model schemas?
 app.post("/register", users.createOne);
-<<<<<<< HEAD
 app.post("/createCard", cards.createOne);
-
-=======
 app.post("/login", users.findOne);
->>>>>>> Davis
 
 // perform a database connection when server starts
 connectDB();

9fe190755731363492610148a1251dfa872273ad
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-04-18 18:18:40 +1200

    Css hover brings up text

--- ark/server/controllers/userController.js
+++ ark/server/controllers/userController.js
@@ -17,6 +17,8 @@ module.exports = {
 
   findOne: async (req, res) => {
     const {email, password} = req.body;
+   
+
 
     if(!email || !password){
       res.status(400).json({success:false, error: "Please provide email and password"})

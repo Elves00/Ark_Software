b19d060d47f5ad792c872273cf1e46bc2b716b43
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-05-05 20:02:12 +1200

    resolving conflict

--- ark/client/src/App.js
+++ ark/client/src/App.js
@@ -16,50 +16,13 @@ import HomePage from "./components/homePage";
 import Profile from "./components/profilePage";
 import Login from "./components/login";
 import Register from "./components/register";
-<<<<<<< HEAD
-// import Private from "./components/routing/auth";
-import EditProfile from "./components/editProfile";
-import Logout from "./components/logout";
-import Footer from "./components/footer/footer";
-import "./App.css"
-=======
 import Private from "./components/routing/auth";
 import EditProfile from "./components/editProfile";
 import Logout from "./components/logout";
->>>>>>> f59a00e32abd760004f6cb6918f705be63a67ce0
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <div className="content">
-        <Navbar />
-        <div style={{ margin: 20 }}>
-          <Routes>
-            {/*First as Not found*/}
-            <Route path='*' element={<NotFound />} />
-            <Route exact path="/" element={<HomePage />} />
-            <Route path="/bossPage" element={<Boss />} />
-            <Route path="/raidPage" element={<Raid />} />
-            {/* <Route exact path="/forumPage" element={<Private />}> */}
-            <Route exact path="/forumPage" element={<Forums />} />
-            {/* </Route> */}
-            <Route path="/chatPage" element={<Chat />} />
-            <Route path="/accountPage" element={<Account />} />
-            <Route path="/profilePage" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-
-            <Route path="/createUser" element={<CreateUser />} />
-            <Route path="/editProfile" element={<EditProfile />} />
-            <Route path="/logout" element={<Logout />} />
-            {/* <Route path="/editUser/:id" element={<EditUser />} /> */}
-          </Routes>
-        </div>
-
-      </div>
-      <Footer />
-=======
       <Navbar />
       <div style={{ margin: 20 }}>
         <Routes>
@@ -83,7 +46,6 @@ const App = () => {
           {/* <Route path="/editUser/:id" element={<EditUser />} /> */}
         </Routes>
       </div>
->>>>>>> f59a00e32abd760004f6cb6918f705be63a67ce0
     </div>
   );
 };

3b9cac93ac873cc955746350b57e649ada6f7c6b
MonkeySki <rebeccafredericknz@gmail.com>
2022-05-17 16:31:12 +1200

    Follow page shows image and text if user exists

--- ark/client/src/components/follow.js
+++ ark/client/src/components/follow.js
@@ -6,7 +6,11 @@ import "./user.css";
 const Login = () => {
   const [user, setUser] = useState("");
   const [error, setError] = useState("");
-  const [retrievedUser, setRetrievedUser] = useState("");
+  const [retrievedUser, setRetrievedUser] = useState({
+    postImage: { myFile: "" },
+  });
+  const [searched, setSearched] = useState("");
+
   // const navigate = useNavigate();
 
   // const config = {
@@ -32,20 +36,72 @@ const Login = () => {
   //     fetchPrivateData();
   //   });
 
-  const searchHandler = async (e) => {
+  async function searchHandler(e) {
     e.preventDefault();
 
     try {
       const { data } = await axios.post(`/follow/${user}`);
       console.log(data.data);
       setRetrievedUser(data.data);
+      setSearched(true);
     } catch (error) {
       setError("Username does not exist!");
       setTimeout(() => {
         setError("");
       }, 3000);
     }
-  };
+  }
+
+  function getPhotoString() {
+    return retrievedUser.postImage.myFile
+  }  
+
+  function showProfile(user){
+
+    if (searched === true){
+      return(<div className="FullProfile">
+      <div class="row">
+        <div class="col-md-10">
+          <div class="row">
+            <div class="col-md-4">
+              <h2 class="h2">{retrievedUser.username}</h2>
+              <img class="col-md-10"
+                src= {getPhotoString()}
+                alt = "ProfilePhoto"
+                />         
+                
+            </div>
+
+            <div class="col-md-8">
+              <h2 class="h2">Character Information</h2>
+              <p style={{textAlign: "left"}}>Class: {retrievedUser.characterClass}</p>
+              {/* <p>Skills: {data.skills}</p>
+                <p>Builds: {data.builds}</p> */}
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-10">
+              <h2 class="h2">About Me</h2>
+              <p style={{textAlign: "left"}}>{retrievedUser.aboutMe}</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          {/* <h2 class="text-center">Friends</h2>
+
+            <p class="text-center">Friends</p>
+            <p class="text-center">Friends</p>
+            <p class="text-center">Friends</p>
+            <p class="text-center">Friends</p>
+            <p class="text-center">Friends</p>
+            <p class="text-center">Friends</p>
+            <p class="text-center">Friends</p> */}
+        </div>
+      </div>
+    </div>)
+    }
+   
+  }
 
   return (
     <div className="form">
@@ -71,9 +127,7 @@ const Login = () => {
         <br />
       </form>
       <div className="result">
-        {retrievedUser.username}
-        {retrievedUser.characterClass}
-        {retrievedUser.aboutMe}
+        {showProfile(retrievedUser.username)}
       </div>
     </div>
   );

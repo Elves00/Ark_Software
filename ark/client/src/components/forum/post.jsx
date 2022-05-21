7cd36183f66c8d6b47bdf9722a9b1cca0768fdaa
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-05-21 19:23:07 +1200

    posting

--- ark/client/src/components/forum/post.jsx
+++ ark/client/src/components/forum/post.jsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Posts(props) {
+
+
+    const [rait, setRait] = useState(0);
+    const [value, setValue] = useState();
+    const [disable, setDisable] = useState(0);
+    const [id, setId] = useState();
+
+    useEffect(() => {
+        console.log(props.props._id)
+        setId(props.props._id)
+        setValue(props.props.raiting);
+    }, []);
+
+    
+    function up() {
+
+
+        if (disable == 1) {
+            return;
+        }
+
+        console.log("The id is :" + id);
+        const config = {
+            header: {
+                "Content-Type": "application/json",
+            },
+        };
+
+
+        try {
+            axios.post("http://localhost:5000/postUp",
+                //The name is used to identify the page to increments
+                { id },
+                config
+            )
+            setValue(value + 1);
+            setDisable(1);
+
+            // alert("Click!");
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    function down() {
+
+        if (disable == -1) {
+            return;
+        }
+        console.log("The id is :" + id);
+        const config = {
+            header: {
+                "Content-Type": "application/json",
+            },
+        };
+
+
+        try {
+            axios.post("http://localhost:5000/postDown",
+                //The name is used to identify the page to increments
+                { id },
+                config
+            )
+            setValue(value - 1);
+            setDisable(-1);
+            // alert("Click!");
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
     return (
         <div className="forum__content">
             <div className="subforum-row">
                 <div className="like-dislike subforum-column">
-                    <p>Vote Up or Down</p>
+                    <button onClick={up}>up</button>
+                    <button onClick={down}>down</button>
+                    <p>{value}</p>
                 </div>
                 <div className="description subforum-column">
                     <h2>
@@ -22,7 +97,7 @@ export default function Posts(props) {
                     <b>
                         <a href="">Last Post</a>
                     </b>{' '}
-                    by <a href="">{props.props.user}</a>
+                    by <a href="">{props.props.name}</a>
                     <br></br>
                     on {props.props.date}
                 </div>
